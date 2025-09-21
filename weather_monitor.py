90e22ce1a0bff05cc87efda4efb83b4d4fb933a6
Timur_23_1337 <66136873+timur-23-1337@users.noreply.github.com>
2025-09-21 20:35:28 +0500

    Fix crashing issue after sleep state

--- weather_monitor.py
+++ weather_monitor.py
@@ -5,21 +5,28 @@ import sys, os, logging, socket, time, requests
 from waveshare_epd import epd2in7
 from PIL import Image,ImageDraw,ImageFont
 #------------------------------------------------------------------------------------------
-API_KEY = ""
-CITY_ID = ""
+API_KEY = ''
+CITY_ID = 
 url = f"http://api.openweathermap.org/data/2.5/weather?id={CITY_ID}&appid={API_KEY}&units=metric&lang=ru"
 #------------------------------------------------------------------------------------------
 picdir = "pic"
 libdir = "lib"
+font = "PTSans.ttf"
 if os.path.exists(libdir):
     sys.path.append(libdir)
 #------------------------------------------------------------------------------------------
 logging.basicConfig(level=logging.ERROR)
 #------------------------------------------------------------------------------------------
-font_small = ImageFont.truetype(f'{picdir}/Font.ttc', 32)
-font_big = ImageFont.truetype(f'{picdir}/Font.ttc', 52)
+font_small = ImageFont.truetype(f'{picdir}/fonts/{font}', 32)
+font_big = ImageFont.truetype(f'{picdir}/fonts/{font}', 52)
 prev_temperature = 0.0
 fisrt_start = 1
+first_line = -6
+second_line = 54
+third_line = 92
+fourth_line = 130
+padding_text_l = 66
+padding_text_s = 62
 #------------------------------------------------------------------------------------------
 thunderstorm = [200, 201, 202, 210, 211, 212, 221, 230, 231, 232]
 drizzle = [300, 301, 302, 310, 311, 312, 313, 314, 321]
@@ -38,13 +45,13 @@ def internet():
         return False
 #------------------------------------------------------------------------------------------
 epd = epd2in7.EPD()
-epd.init()
-epd.Clear(0xFF)
 Himage = Image.new('1', (epd.height, epd.width), 255)
 draw = ImageDraw.Draw(Himage)
 #------------------------------------------------------------------------------------------
 try:
     while True:
+        epd.init()
+        epd.Clear(0xFF)
         Himage = Image.new('1', (epd.height, epd.width), 255)
         draw = ImageDraw.Draw(Himage)
         response = requests.get(url)
@@ -55,10 +62,10 @@ try:
             Himage.paste(Image.open(os.path.join(picdir, 'none.bmp')), (22,58))
             Himage.paste(Image.open(os.path.join(picdir, 'none.bmp')), (22,96))
             Himage.paste(Image.open(os.path.join(picdir, 'none.bmp')), (22,134))
-            draw.text((66, 0), 'Error:', font = font_big, fill = 0)
-            draw.text((62, 58), 'No Wi-Fi', font = font_small, fill = 0) 
-            draw.text((62, 96), 'connection', font = font_small, fill = 0) 
-            draw.text((62, 134), 'available', font = font_small, fill = 0) 
+            draw.text((padding_text_l, first_line), 'Error:', font = font_big, fill = 0)
+            draw.text((padding_text_s, second_line), 'No Wi-Fi', font = font_small, fill = 0) 
+            draw.text((padding_text_s, third_line), 'connection', font = font_small, fill = 0) 
+            draw.text((padding_text_s, fourth_line), 'available', font = font_small, fill = 0) 
             epd.display(epd.getbuffer(Himage))
             epd.sleep()
             time.sleep(60)
@@ -74,38 +81,35 @@ try:
             Himage.paste(Image.open(os.path.join(picdir, 'none.bmp')), (22,58))
             Himage.paste(Image.open(os.path.join(picdir, 'none.bmp')), (22,96))
             Himage.paste(Image.open(os.path.join(picdir, 'none.bmp')), (22,134))
-            draw.text((66, 0), 'Error:', font = font_big, fill = 0)
-            draw.text((62, 58), 'Got bad', font = font_small, fill = 0) 
-            draw.text((62, 96), 'response', font = font_small, fill = 0) 
-            draw.text((62, 134), f'code: {response.status_code}', font = font_small, fill = 0) 
+            draw.text((padding_text_l, first_line), 'Error:', font = font_big, fill = 0)
+            draw.text((padding_text_s, second_line), 'Got bad', font = font_small, fill = 0) 
+            draw.text((padding_text_s, third_line), 'response', font = font_small, fill = 0) 
+            draw.text((padding_text_s, fourth_line), f'code: {response.status_code}', font = font_small, fill = 0) 
             epd.display(epd.getbuffer(Himage))
             epd.sleep()
             time.sleep(60)
         #------------------------------------------------------------------------------------------
         if weather_id in thunderstorm:
-            draw.text((66, 0), 'Гроза', font = font_big, fill = 0)
+            draw.text((padding_text_l, first_line), 'Гроза', font = font_big, fill = 0)
         elif weather_id in drizzle:
-            draw.text((66, 0), 'Морось', font = font_big, fill = 0)
+            draw.text((padding_text_l, first_line), 'Морось', font = font_big, fill = 0)
         elif weather_id in rain:
-            draw.text((66, 0), 'Дождь', font = font_big, fill = 0)
+            draw.text((padding_text_l, first_line), 'Дождь', font = font_big, fill = 0)
         elif weather_id in snow:
-            draw.text((66, 0), 'Снег', font = font_big, fill = 0)
+            draw.text((padding_text_l, first_line), 'Снег', font = font_big, fill = 0)
         elif weather_id in mist:
-            draw.text((66, 0), 'Туман', font = font_big, fill = 0)
+            draw.text((padding_text_l, first_line), 'Туман', font = font_big, fill = 0)
         elif weather_id in clear:
-            draw.text((66, 0), 'Ясно', font = font_big, fill = 0)
+            draw.text((padding_text_l, first_line), 'Ясно', font = font_big, fill = 0)
         elif weather_id in clouds:
-            draw.text((66, 0), 'Облач.', font = font_big, fill = 0)
+            draw.text((padding_text_l, first_line), 'Облач.', font = font_big, fill = 0)
         else:
-            draw.text((66, 0), 'ERROR', font = font_big, fill = 0)
+            draw.text((padding_text_l, first_line), 'ERROR', font = font_big, fill = 0)
         #---
         if os.path.isfile(f'{picdir}/{weather_icon}.bmp'):
             Himage.paste(Image.open(os.path.join(picdir, f'{weather_icon}.bmp')), (6,2))
         else:
             Himage.paste(Image.open(os.path.join(picdir, 'error.bmp')), (6,2))
-        #------------------------------------------------------------------------------------------
-        if prev_temperature == 0.0:
-            prev_temperature = temperature
         #------------------------------------------------------------------------------------------
         if fisrt_start == 1:
             fisrt_start = 0
@@ -128,9 +132,9 @@ try:
             Himage.paste(Image.open(os.path.join(picdir, 'humidity_mid.bmp')), (22,134))
         #------------------------------------------------------------------------------------------
         prev_temperature = temperature
-        draw.text((62, 58), f'{temperature} °С', font = font_small, fill = 0) 
-        draw.text((62, 96), f'{wind_speed} м/с', font = font_small, fill = 0) 
-        draw.text((62, 134), f'{humidity}%', font = font_small, fill = 0) 
+        draw.text((padding_text_s, second_line), f'{temperature} °С', font = font_small, fill = 0) 
+        draw.text((padding_text_s, third_line), f'{wind_speed} м/с', font = font_small, fill = 0) 
+        draw.text((padding_text_s, fourth_line), f'{humidity}%', font = font_small, fill = 0) 
         #------------------------------------------------------------------------------------------
         epd.display(epd.getbuffer(Himage))
         epd.sleep()
